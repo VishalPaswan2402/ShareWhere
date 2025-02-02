ee39b9acf0e1bf9757a597492388c30da14013eb
Vishal Paswan <vishalvp.3405@gmail.com>
2025-02-02 09:46:24 +0530

    resolve git error

--- frontend/src/components/Button/Button.jsx
+++ frontend/src/components/Button/Button.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import './Button.css'
 
-export default function Button({ label, icon }) {
+export default function Button({ label, icon, click }) {
 
 
     return (
         <>
-            <button type='submit' className='submit-btn'>{label} &nbsp; <i className={icon}></i> </button>
+            <button type='submit' className='submit-btn' onClick={click}>{label} &nbsp; <i className={icon}></i> </button>
         </>
     )
 }
--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -2,16 +2,25 @@ import React from 'react'
 import Logo from '../../components/Logo/Logo'
 import Button from '../../components/Button/Button'
 import './HomePage.css'
+import { useNavigate } from 'react-router-dom';
+import DownloadPage from '../DownloadPage/DownloadPage';
 
 export default function HomePage(props) {
-
-
+    const navigate = useNavigate();
+    const directToSharePage = () => {
+        console.log("Share page");
+        navigate("/uploadFile");
+    }
+    const directToDownloadPage = () => {
+        console.log("DownloadPage");
+        navigate("/downloadFile");
+    }
     return (
         <>
             <Logo />
-            <Button label="Share File" icon="fa-solid fa-cloud-arrow-up" />
+            <Button label="Share File" icon="fa-solid fa-cloud-arrow-up" click={directToSharePage} />
             <p className='differ'>--------- ---------</p>
-            <Button label="Receive File" icon="fa-solid fa-cloud-arrow-down" />
+            <Button label="Receive File" icon="fa-solid fa-cloud-arrow-down" click={directToDownloadPage} />
         </>
     )
 }
